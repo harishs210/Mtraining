c1bb79cba6984e6761b9ba685d6e7011661a3306
Harish S <harishsahoo21@gmail.com>
2023-01-16 09:46:48 +0000

    Added spring security

--- ums-api/src/main/java/com/lti/umsapi/ctrl/UserController.java
+++ ums-api/src/main/java/com/lti/umsapi/ctrl/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.umsapi.dto.UserDto;
@@ -18,6 +19,7 @@ import com.lti.umsapi.service.UserService;
 @RestController
 
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
 public class UserController {
 	@Autowired
 	UserService userService;
---
+++ ums-api/src/main/java/com/lti/umsapi/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.lti.umsapi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.inMemoryAuthentication()
+		.withUser("harish").password(passwordEncoder().encode("harish123")).roles("ADMIN")
+		.and()
+		.withUser("Ramesh").password(passwordEncoder().encode("ramesh123")).roles("USER");
+
+//		passwordEncoder().encode("harish123")
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("api/users/**").hasRole("ADMIN").anyRequest().authenticated()
+		.and().httpBasic();
+		
+		http.csrf().disable();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
