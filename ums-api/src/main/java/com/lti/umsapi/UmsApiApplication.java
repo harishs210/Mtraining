dc53f46455829017bfb5d8e7a252b551ce531d22
Harish S <harishsahoo21@gmail.com>
2023-01-17 13:21:32 +0000

    added eureka client

--- ums-api/src/main/java/com/lti/umsapi/UmsApiApplication.java
+++ ums-api/src/main/java/com/lti/umsapi/UmsApiApplication.java
@@ -2,8 +2,10 @@ package com.lti.umsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UmsApiApplication {
 
 	public static void main(String[] args) {
